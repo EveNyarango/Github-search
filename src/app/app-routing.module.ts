3044e49336dc45c072f1c1ce2f52897562306828
EveNyarango <evenyarango@gmail.com>
2020-11-15 16:52:52 +0300

    add routing function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { SearchComponent } from './search/search.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: SearchComponent},
+  { path: 'repo', component: SearchComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
